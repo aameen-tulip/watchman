eeb1fa543193c5985a9ec284470c700096c76893
Alex Ameen <alex.ameen@tulip.co>
2022-06-09 15:25:04 -0500

    add flake, kill part of autogen.sh that downloads shit

--- autogen.sh
+++ autogen.sh
@@ -7,8 +7,8 @@
 
 set -x
 PREFIX=${PREFIX:-/usr/local}
-python3 build/fbcode_builder/getdeps.py build --src-dir=. watchman \
-  "--project-install-prefix=watchman:$PREFIX"
+#python3 build/fbcode_builder/getdeps.py build --src-dir=. watchman \
+#  "--project-install-prefix=watchman:$PREFIX"
 python3 build/fbcode_builder/getdeps.py fixup-dyn-deps \
   --src-dir=. watchman  built \
   "--project-install-prefix=watchman:$PREFIX" \
---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs.util.url = "github:numtide/flake-utils/master";
+  inputs.util.inputs.nixpkgs.follows = "/nixpkgs";
+
+  inputs.fbthrift = {
+    url = "github:facebook/fbthrift/5e4ce0994de546bc7e17a0ba588518cabd01f303";
+    flake = false;
+  };
+
+  inputs.folly = {
+    url = "github:facebook/folly/530a7266f9f15eaad196ce1b291fbbfc8e578810";
+    flake = false;
+  };
+
+  inputs.wangle = {
+    url = "github:facebook/wangle/6edb5d93b6770db56b23c670b782ee452a844b54";
+    flake = false;
+  };
+
+
+  outputs = { self, nixpkgs, util, /*watchman,*/ fbthrift, folly, wangle }: let
+    inherit (util.lib) eachDefaultSystemMap;
+  in {
+    packages = eachDefaultSystemMap ( system: let
+      pkgsFor = nixpkgs.legacyPackages.${system};
+    in {
+      watchman = pkgsFor.stdenv.mkDerivation {
+        pname = "watchman";
+        version = "2022.03.21";
+        src = self;
+        nativeBuildInputs = with pkgsFor; [
+          cmake autoconf automake pkg-config libtool python3
+        ];
+        buildInputs = with pkgsFor; [
+          pcre openssl gmock glog boost.dev libevent
+          fmt pkgsFor.folly
+          fizz
+        ]; # CoreServices for Apple
+
+        prePatch = ''
+          rm -f ./autogen.sh ./autogen.cmd
+          cp -r --reflink=auto -- ${fbthrift} ./build/deps/fbthrift
+          cp -r --reflink=auto -- ${folly}    ./build/deps/folly
+          cp -r --reflink=auto -- ${wangle}   ./build/deps/wangle
+          chmod -R u+w ./build/deps
+          patchShebangs .
+        '';
+
+        # FIXME: convert to CMake equivalents...
+        configureFlags = [
+          "--enable-lenient"
+          "--with-pcre=yes"
+          "--disable-statedir"
+        ];
+
+        configurePhase = ''
+          runHook preConfigure
+          cmake .
+          runHook postConfigure
+        '';
+
+      };
+      default = self.packages.${system}.watchman;
+    } );
+  };
+}
