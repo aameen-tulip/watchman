e226524c3a034c132a4f290b6372b48a322f827f
Alex Ameen <alex.ameen@tulip.co>
2022-06-09 15:36:26 -0500

    f

--- flake.nix
+++ flake.nix
@@ -7,10 +7,12 @@
     flake = false;
   };
 
+  /*
   inputs.folly = {
     url = "github:facebook/folly/530a7266f9f15eaad196ce1b291fbbfc8e578810";
     flake = false;
   };
+  */
 
   inputs.wangle = {
     url = "github:facebook/wangle/6edb5d93b6770db56b23c670b782ee452a844b54";
@@ -18,7 +20,14 @@
   };
 
 
-  outputs = { self, nixpkgs, util, /*watchman,*/ fbthrift, folly, wangle }: let
+  outputs = {
+    self
+    , nixpkgs
+    , util
+    /* , watchman-src  FIXME: replace with upstream repo when we're done hacking around */
+    , fbthrift
+    /* , folly */
+    , wangle }: let
     inherit (util.lib) eachDefaultSystemMap;
   in {
     packages = eachDefaultSystemMap ( system: let
@@ -34,17 +43,19 @@
         buildInputs = with pkgsFor; [
           pcre openssl gmock glog boost.dev libevent
           fmt pkgsFor.folly
-          fizz
+          # FIXME: no idea there this tool comes from  -  fizz
         ]; # CoreServices for Apple
 
         prePatch = ''
           rm -f ./autogen.sh ./autogen.cmd
           cp -r --reflink=auto -- ${fbthrift} ./build/deps/fbthrift
-          cp -r --reflink=auto -- ${folly}    ./build/deps/folly
           cp -r --reflink=auto -- ${wangle}   ./build/deps/wangle
           chmod -R u+w ./build/deps
           patchShebangs .
         '';
+        # I'm going to try to use `nixpkgs.folly' first, but if it fails
+        # we can let them build it from scratch like they normally do.
+        #cp -r --reflink=auto -- ${folly}    ./build/deps/folly
 
         # FIXME: convert to CMake equivalents...
         configureFlags = [
